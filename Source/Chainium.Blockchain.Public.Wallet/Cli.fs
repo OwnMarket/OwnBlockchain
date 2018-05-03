be1d114cb4d38577a08c09348a0c20376ff6a3bb
Anil Mujagic <anil.mujagic@chainium.io>
2018-05-03 10:30:23 +0200

    Define and handle wallet CLI commands

---
+++ Source/Chainium.Blockchain.Public.Wallet/Cli.fs
@@ -0,0 +1,36 @@
+namespace Chainium.Blockchain.Public.Wallet
+
+open System
+open System.Reflection
+open Chainium.Blockchain.Public.Core.DomainTypes
+open Chainium.Blockchain.Public.Crypto
+
+module Cli =
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Handlers
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    let handleGenerateKeyPairCommand seed =
+         Signing.generateKeyPair seed
+         |> printfn "Key Pair: %A" // TODO: Decide about the output format
+
+    let handleSignMessageCommand privateKey message =
+         Signing.signMessage privateKey message
+         |> printfn "Signature: %A" // TODO: Decide about the output format
+
+    let handleUnknownCommand args =
+        // TODO: Show help
+        printfn "TODO: Print short command reference"
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Configuration
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    let handleCommand args =
+        match args with
+        | ["-g"] -> handleGenerateKeyPairCommand None
+        | ["-g"; seed] -> handleGenerateKeyPairCommand (Some seed)
+        | ["-s"; privateKey; message] -> handleSignMessageCommand (PrivateKey privateKey) message
+        | _ -> handleUnknownCommand args
--- Source/Chainium.Blockchain.Public.Wallet/Program.fs
+++ Source/Chainium.Blockchain.Public.Wallet/Program.fs
@@ -1,5 +1,18 @@
 ﻿open System
+open System.Globalization
+open System.Threading
+open Chainium.Common
+open Chainium.Blockchain.Common
+open Chainium.Blockchain.Public.Wallet
 
 [<EntryPoint>]
 let main argv =
+    try
+        Thread.CurrentThread.CurrentCulture <- CultureInfo.InvariantCulture
+        Thread.CurrentThread.CurrentUICulture <- CultureInfo.InvariantCulture
+
+        argv |> Array.toList |> Cli.handleCommand
+    with
+    | ex -> Log.error ex.AllMessagesAndStackTraces
+
     0 // Exit code
