e5b5ee1ec6c1c72e8b1db51b8e5d2a5f6a93cca9
Anil Mujagic <anil.mujagic@chainium.io>
2018-05-03 12:14:41 +0200

    Configure API

---
+++ Source/Chainium.Blockchain.Public.Node/Api.fs
@@ -0,0 +1,39 @@
+﻿namespace Chainium.Blockchain.Public.Node
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+module Api =
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Configuration
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    let api =
+        choose [
+            GET >=> choose [
+                route "/" >=> text "TODO: Show link to the help page"
+            ]
+            POST >=> choose [
+                route "/tx" >=> text "TODO: Handle Tx submission"
+            ]
+        ]
+
+    let configureApp (app : IApplicationBuilder) =
+        // Add Giraffe to the ASP.NET Core pipeline
+        app.UseGiraffe api
+
+    let configureServices (services : IServiceCollection) =
+        // Add Giraffe dependencies
+        services.AddGiraffe() |> ignore
+
+    let start () =
+        WebHostBuilder()
+            .UseKestrel()
+            .Configure(Action<IApplicationBuilder> configureApp)
+            .ConfigureServices(configureServices)
+            .Build()
+            .Run()
--- Source/Chainium.Blockchain.Public.Node/Program.fs
+++ Source/Chainium.Blockchain.Public.Node/Program.fs
@@ -1,5 +1,18 @@
 ﻿open System
+open System.Globalization
+open System.Threading
+open Chainium.Common
+open Chainium.Blockchain.Common
+open Chainium.Blockchain.Public.Node
 
 [<EntryPoint>]
 let main argv =
+    try
+        Thread.CurrentThread.CurrentCulture <- CultureInfo.InvariantCulture
+        Thread.CurrentThread.CurrentUICulture <- CultureInfo.InvariantCulture
+
+        Api.start ()
+    with
+    | ex -> Log.error ex.AllMessagesAndStackTraces
+
     0 // Exit code
