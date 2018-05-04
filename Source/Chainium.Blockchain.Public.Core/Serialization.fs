a0b1f91b149a900c76646f8ebd00065d1b08a2d6
Anil Mujagic <anil.mujagic@chainium.io>
2018-05-05 01:28:12 +0200

    Add Tx submission workflow skeleton

---
+++ Source/Chainium.Blockchain.Public.Core/Serialization.fs
@@ -0,0 +1,33 @@
+﻿namespace Chainium.Blockchain.Public.Core
+
+open System
+open System.Text
+open Chainium.Common
+open Chainium.Blockchain.Common
+open Chainium.Blockchain.Public.Core.DomainTypes
+open Chainium.Blockchain.Public.Core.Dtos
+
+module Serialization =
+
+    let stringToBytes (str : string) =
+        Encoding.UTF8.GetBytes(str)
+
+    let bytesToString (bytes : byte[]) =
+        Encoding.UTF8.GetString(bytes)
+
+    let deserializeSignedTx (signedTx : string) : Result<SignedTx, AppErrors> =
+        match signedTx.Split(";") with
+        | [| rawTx; signaturePartR; signaturePartS |] ->
+            Ok {
+                RawTx = Convert.FromBase64String(rawTx)
+                Signature = { R = signaturePartR; S = signaturePartS }
+            }
+        | _ -> Error [AppError "SignedTx is expected to have three parts separated by a semicolon ';'."]
+
+    let deserializeTx (rawTx : byte[]) : Result<TxDto, AppErrors> =
+        // TODO: Implement
+        Ok {
+            Nonce = 1L
+            Actions = []
+            Fee = 1M
+        }
---
+++ Source/Chainium.Blockchain.Public.Core/Validation.fs
@@ -0,0 +1,26 @@
+﻿namespace Chainium.Blockchain.Public.Core
+
+open System
+open Chainium.Common
+open Chainium.Blockchain.Common
+open Chainium.Blockchain.Public.Core.DomainTypes
+open Chainium.Blockchain.Public.Core.Dtos
+
+module Validation =
+
+    let verifyTxSignature verifySignature (signedTx : SignedTx) : Result<ChainiumAddress * TxHash, AppErrors> =
+        match verifySignature signedTx.Signature signedTx.RawTx with
+        | Some address ->
+            Ok (address, TxHash "DUMMY_HASH") // TODO: Implement
+        | None ->
+            Error [AppError "Cannot verify signature"]
+
+    let validateTx sender hash (txDto : TxDto) : Result<Tx, AppErrors> =
+        // TODO: Implement
+        Ok {
+            TxHash = hash
+            Sender = sender
+            Nonce = txDto.Nonce
+            Actions = []
+            Fee = ChxAmount txDto.Fee
+        }
