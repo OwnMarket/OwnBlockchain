60c6254088994df2f08f146f1011b6d3a50f56c8
mrpink <sead.pavica@chainium.io>
2018-05-16 15:04:15 +0200

    added functions for verification and proof generation

--- Source/Chainium.Blockchain.Public.Crypto.Tests/MerkleTreeTests.fs
+++ Source/Chainium.Blockchain.Public.Crypto.Tests/MerkleTreeTests.fs
@@ -23,10 +23,82 @@ module MerkleTreeTests =
             test <@ leafs.Length = root.Length @>
 
     [<Fact>]
-    let ``MerkleTree.build - check for empty list`` ()=            
+    let ``MerkleTree.build - check empty list creation`` () =            
             let root = MerkleTree.build hashFunc []
             test <@ root.Length = 0 @>
                 
-                
-                            
+    [<Fact>]
+    let ``MerkleTree.calculateProof - get proof`` () =
+        let leafs = 
+                [
+                    for i in 1 .. 4 do yield [| byte(i) |]
+                ]    
+        let record = [| byte(2) |]
+        let expected = 
+            [
+                LeftHash [| byte(1) |]
+                RightHash [| byte(3); byte(4) |]
+            ] 
+        
+        let actual = MerkleTree.calculateProof hashFunc leafs record
 
+        test <@ expected = actual @>
+    
+    [<Fact>]
+    let ``MerkleTree.calculateProof - uneven nodes get proof`` () =
+        let leafs = 
+                [
+                    for i in 1 .. 5 do yield [| byte(i) |]
+                ]    
+        let record = [| byte(1) |]
+        let expected = 
+            [
+                RightHash [| byte(2) |]
+                RightHash [| byte(3) |]
+                RightHash [| byte(4); byte(5) |]
+            ] 
+        
+        let actual = MerkleTree.calculateProof hashFunc leafs record
+
+        test <@ expected = actual @>
+
+    [<Fact>]
+    let ``MerkleTree.calculateProof - non existing leaf hash`` () =
+        let leafs = 
+                [
+                    for i in 1 .. 4 do yield [| byte(i) |]
+                ]    
+        let record = [| byte(0) |]
+        
+        let expected = List.Empty
+        let actual = MerkleTree.calculateProof hashFunc leafs record
+
+        test <@ expected = actual @>
+                            
+    [<Fact>]
+    let ``MerkleTree.verifyProof - leaf hash`` () =
+        let root = [| for i in 1 .. 4 do yield byte(i) |]
+        let record = [| byte(2) |]
+        let proof = 
+            [
+                LeftHash [| byte(1) |]
+                RightHash [| byte(3); byte(4) |]
+            ]
+        
+        let isVerified = MerkleTree.verifyProof hashFunc record proof root
+
+        test <@ isVerified @>
+
+    [<Fact>]
+    let ``MerkleTree.verifyProof - invalid leaf hash`` () =
+        let root = [| for i in 1 .. 4 do yield byte(i) |]
+        let record = [| byte(5) |]
+        let proof = 
+            [
+                LeftHash [| byte(1) |]
+                RightHash [| byte(3); byte(4) |]
+            ]
+        
+        let isVerified = MerkleTree.verifyProof hashFunc record proof root
+
+        test <@ isVerified = false @>
--- Source/Chainium.Blockchain.Public.Crypto/MerkleTree.fs
+++ Source/Chainium.Blockchain.Public.Crypto/MerkleTree.fs
@@ -1,19 +1,28 @@
 ﻿namespace Chainium.Blockchain.Public.Crypto
 
 module MerkleTree =
-    
     type private MerkleNode =
         {
+            mutable Parent : MerkleNode option
             Hash : byte[]
             Left : MerkleNode option
             Right : MerkleNode option
         }
+    
     
     let private nodehash x =
         match x with
         | Some l -> l.Hash
         | None -> Array.zeroCreate 0
 
+    
+    let private concatHashes 
+        left
+        right 
+        =
+        Array.append left right
+
+    
     let private buildNode
         hashFunction
         (left : MerkleNode option) 
@@ -22,16 +31,32 @@ module MerkleTree =
         let lefthash = nodehash left
         let righthash = nodehash right
 
-        {
-            Left = left
-            Right = right
-            Hash =  
-                righthash
-                |> Array.append lefthash
-                |> hashFunction
-        }
-        |> Some
 
+        let setParent node parent=
+            
+            match node with
+            | Some x -> 
+                x.Parent <- Option.map (fun m->m) parent
+            | None -> ()
+
+        let node =
+            {
+                Parent = None
+                Left = left
+                Right = right
+                Hash =  
+                    righthash
+                    |> Array.append lefthash
+                    |> hashFunction
+            }
+        
+        let nodeResult = node |> Some
+        
+        setParent node.Left nodeResult
+        setParent node.Right nodeResult
+        
+        nodeResult
+        
     
     let rec private buildTree 
         hashFunction 
@@ -74,19 +99,127 @@ module MerkleTree =
             |> List.map(fun s -> buildSubTree s)
             |> treeBuilder
 
-
-
-    let build hashFunc leafHashes =  
+    
+    let private buildNodes leafHashes =
         leafHashes
         |> List.map
             (
                 fun h -> 
                     {
+                        Parent = None
                         Hash = h
                         Left = None
                         Right = None
                     } 
                     |> Some
             )
+
+
+    let build hashFunc leafHashes =  
+        leafHashes
+        |> buildNodes
         |> buildTree hashFunc
         |> nodehash
+
+    
+    let rec private findLeaf 
+        (node : MerkleNode option)
+        (hash : byte[])
+        =
+        let searchSubTree x =
+            if 
+                x.Left = None 
+                && x.Right = None 
+                && x.Hash = hash 
+            then
+                node
+            else
+                let left = findLeaf x.Left hash
+                let right = findLeaf x.Right hash
+
+                match left with
+                | Some x -> left
+                | None -> right
+
+        Option.bind (fun n -> searchSubTree(n)) node
+   
+
+    type MerkleProofSegment = 
+        | LeftHash of byte[]
+        | RightHash of byte[]
+    
+    type MerkleProof = MerkleProofSegment list
+    
+
+    let rec private merkleProof node segments hash =
+        let otherSubtreeHash parent =
+            
+            let childHash c=
+                match c with
+                | None -> Array.zeroCreate 0
+                | Some c -> c.Hash
+
+            let leftHash = childHash parent.Left
+            let rightHash = childHash parent.Right
+            
+            if leftHash = hash then
+                RightHash rightHash
+            elif rightHash = hash then
+                LeftHash leftHash
+            else
+                LeftHash (Array.zeroCreate 0)
+
+        match node.Parent with
+        | None -> segments
+        
+        | Some p ->
+            let newSegments = 
+                [ otherSubtreeHash p ]
+                |> List.append segments
+            
+            merkleProof p newSegments p.Hash  
+
+
+
+    let calculateProof
+        hashFunc 
+        leafHashes
+        leafHash 
+        : MerkleProof
+        =
+            let root = 
+                leafHashes
+                |> buildNodes
+                |> buildTree hashFunc
+
+            let leaf = findLeaf root leafHash
+            
+            match leaf with
+            | None -> [] 
+            | Some l -> merkleProof l [] leafHash
+
+    let verifyProof
+        hashFunc
+        leafHash
+        proof
+        merkleRoot
+        =  
+        
+        let hashFromSegment hash segment =
+            let concated = 
+                match segment with
+                | LeftHash l -> concatHashes l hash
+                | RightHash r -> concatHashes hash r
+            
+            hashFunc concated
+        
+        let calculatedProof = 
+            proof
+            |> List.fold (fun h x -> hashFromSegment h x) leafHash
+        
+        merkleRoot = calculatedProof
+        
+        
+        
+            
+
