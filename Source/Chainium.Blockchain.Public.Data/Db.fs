dd763f1814d807b8bc9c8bea45832d1392dcb04b
mrpink <sead.pavica@chainium.io>
2018-05-19 09:20:27 +0200

    added functions for transaction storing and transactions retreieval

--- Source/Chainium.Blockchain.Public.Data/Db.fs
+++ Source/Chainium.Blockchain.Public.Data/Db.fs
@@ -7,12 +7,45 @@ open Chainium.Blockchain.Public.Core.DomainTypes
 open Chainium.Blockchain.Public.Core.Dtos
 
 module Db =
+    let private dbParams (paramsList : (string * obj) list) =
+        let paramsString = 
+            paramsList
+            |> List.fold (fun acc (nm,x) -> sprintf "%s, %s" nm acc) ""
+        
+        let paramNames = paramsString.Trim([| ',' ; ' ' |])
+        (paramNames, paramNames.Replace("@",""))
+
+        
 
     let saveTx (dbConnectionString : string) (txInfoDto : TxInfoDto) : Result<unit, AppErrors> =
         try
-            // insert into DB
-            failwith "TODO"
-            Ok ()
+            let txParams =
+                [
+                "@tx_hash", txInfoDto.TxHash |> box;
+                "@sender_address", txInfoDto.SenderAddress |> box;
+                "@nonce", txInfoDto.Nonce |> box;
+                "@fee", txInfoDto.Fee |> box;
+                "@status", txInfoDto.Status |> box
+                ]
+            
+            let paramData = dbParams txParams
+            
+
+            let insertSql =
+                sprintf 
+                    """
+                    INSERT INTO tx (%s)
+                    VALUES (%s)
+                    """ 
+                    (snd paramData)
+                    (fst paramData)
+            
+            let result = DbTools.execute dbConnectionString insertSql txParams
+            
+            if result < 0 then
+                failwith "Unknown error"
+            else
+                Ok ()
         with
         | ex ->
             Log.error ex.AllMessagesAndStackTraces
@@ -25,13 +58,25 @@ module Db =
         : PendingTxInfoDto list
         =
 
+        //TODO : add condition to skip transactions
         let sql =
             """
-            SELECT tx_hash, sender, nonce, fee, tx_id as appearance_order
+            SELECT tx_hash, sender_address, nonce, fee, tx_id as appearance_order
             FROM tx
-            WHERE tx_hash NOT IN (txsToSkip)
+            --WHERE tx_hash NOT IN (@txsToSkip)
             ORDER BY fee, tx_id
-            LIMIT txCountToFetch;
+            LIMIT @txCountToFetch
             """
+        
 
-        failwith "TODO"
+        let sqlParams = 
+            [
+                "@txCountToFetch", txCountToFetch |> box;
+                "@txsToSkip", 
+                txsToSkip
+                |> List.map(fun (TxHash t) -> t)
+                |> List.toSeq
+                |> box;           
+            ]
+
+        DbTools.query dbConnectionString sql sqlParams
--- Source/Chainium.Blockchain.Public.Data/DbTools.fs
+++ Source/Chainium.Blockchain.Public.Data/DbTools.fs
@@ -1,12 +1,101 @@
 namespace Chainium.Blockchain.Public.Data
 
+open Microsoft.Data.Sqlite
+open System.Data.Common
+open Dapper
+open System.Data
 open System
 open Chainium.Common
 
 module DbTools =
+    let private newConnection dbConnectionString =
+        new SqliteConnection(dbConnectionString)
+        :> DbConnection
+
+
+    let private newCommand (sql : string) (conn : DbConnection) : DbCommand =
+        match conn with
+        | :? SqliteConnection as sqlliteconn -> 
+            new SqliteCommand(sql, sqlliteconn)
+            :> DbCommand
+
+        | _ -> failwith "Unknon connection type"
+    
+    let private dbParameter (name : string, value : obj) =
+         SqliteParameter(name, value)
+        :> DbParameter
+        
+
 
     let execute (dbConnectionString : string) (sql : string) (parameters : (string * obj) seq) : int =
-        failwith "Execute SQL and return number of affected rows"
+        use conn = newConnection(dbConnectionString)
+        try
+            use cmd = newCommand sql conn
+
+            let sqlParam = fun (name, value) -> dbParameter(name, value)
+
+            for p in parameters do
+                let queryParam = sqlParam p
+                cmd.Parameters.Add queryParam
+                |> ignore          
+
+            conn.Open()
+            cmd.ExecuteNonQuery()
+        finally
+            conn.Close()
+
+    type private DbParams () =
+        let parameters = System.Collections.Generic.List<IDbDataParameter>()
+
+        interface System.Collections.Generic.IEnumerable<IDbDataParameter> with
+            member this.GetEnumerator () 
+                : System.Collections.Generic.IEnumerator<IDbDataParameter> 
+                =
+                    parameters.GetEnumerator() 
+                    :> System.Collections.Generic.IEnumerator<IDbDataParameter>
+            
+            member this.GetEnumerator () 
+                : System.Collections.IEnumerator 
+                =
+                    parameters.GetEnumerator()
+                    :> System.Collections.IEnumerator
+        
+
+
+        interface SqlMapper.IDynamicParameters with 
+            member this.AddParameters 
+                (
+                    (command  : IDbCommand),
+                    (identity : SqlMapper.Identity)
+                )
+                : unit
+                =
+                    for p in parameters do
+                        command.Parameters.Add p
+
+                        |> ignore
+        
+
+        member this.Add value =
+            parameters.Add(value)
+
+
 
     let query<'T> (dbConnectionString : string) (sql : string) (parameters : (string * obj) seq) : 'T list =
-        failwith "Run Dapper query and return mapped DTOs"
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores <- true
+        use conn = newConnection(dbConnectionString)
+        try
+            let dbParams = DbParams()
+            let addSqlParam = 
+                fun (name, value) -> 
+                    dbParameter(name, value)
+                    |> dbParams.Add 
+
+            parameters
+            |> Seq.iter addSqlParam
+
+            conn.Open()
+            conn.Query<'T>(sql, dbParams)
+            |> List.ofSeq
+        finally
+            conn.Close()
