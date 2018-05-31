275f6ed7b5871e054601e5dd2991c7af3b8c979e
Anil Mujagic <anil.mujagic@chainium.io>
2018-05-31 12:57:24 +0200

    Complete block tests

--- Source/Chainium.Blockchain.Public.Core.Tests/BlocksTests.fs
+++ Source/Chainium.Blockchain.Public.Core.Tests/BlocksTests.fs
@@ -13,7 +13,149 @@ open Chainium.Blockchain.Public.Crypto
 module BlocksTests =
 
     [<Fact>]
+    let ``Blocks.createTxResultHash`` () =
+        let txHash = TxHash "ABC"
+        let txStatus = Success // Code 1 = A
+
+        // ACT
+        let txResultHash = Blocks.createTxResultHash DummyHash.decode DummyHash.create (txHash, txStatus)
+
+        // ASSERT
+        test <@ txResultHash = "ABCA" @>
+
+    [<Fact>]
+    let ``Blocks.createChxBalanceStateHash`` () =
+        let address = ChainiumAddress "ABC"
+        let state = {ChxBalanceState.Amount = ChxAmount 1M; Nonce = Nonce 2L}
+
+        // ACT
+        let stateHash = Blocks.createChxBalanceStateHash DummyHash.decode DummyHash.create (address, state)
+
+        // ASSERT
+        test <@ stateHash = "ABC...A...................B" @>
+
+    [<Fact>]
+    let ``Blocks.createHoldingStateHash`` () =
+        let account = AccountHash "HHH"
+        let equity = EquityID "EE" // E => 69 % 10 => 9 => I
+        let state = {HoldingState.Amount = EquityAmount 7M; Nonce = Nonce 4L}
+
+        // ACT
+        let stateHash = Blocks.createHoldingStateHash DummyHash.decode DummyHash.create (account, equity, state)
+
+        // ASSERT
+        test <@ stateHash = "HHHII...G...................D" @>
+
+    [<Fact>]
+    let ``Blocks.createBlockHash`` () =
+        let blockNumber = BlockNumber 1L
+        let previousBlockHash = BlockHash "B"
+        let timestamp = Timestamp 3L
+        let validator = ChainiumAddress "D"
+        let txSetRoot = MerkleTreeRoot "E"
+        let txResultSetRoot = MerkleTreeRoot "F"
+        let stateRoot = MerkleTreeRoot "G"
+
+        // ACT
+        let (BlockHash blockHash) =
+            Blocks.createBlockHash
+                DummyHash.decode
+                DummyHash.create
+                blockNumber
+                previousBlockHash
+                timestamp
+                validator
+                txSetRoot
+                txResultSetRoot
+                stateRoot
+
+        // ASSERT
+        test <@ blockHash = ".......AB.......CDEFG" @>
+
+    [<Fact>]
     let ``Blocks.assembleBlock`` () =
+        let blockNumber = BlockNumber 1L
+        let previousBlockHash = BlockHash "B"
+        let timestamp = Timestamp 3L
+        let validator = ChainiumAddress "D"
+
+        let txSet =
+            ["AAA"; "BBB"; "CCC"]
+            |> List.map TxHash
+
+        let txResults =
+            [Success; Failure []; Success]
+            |> List.zip txSet
+            |> Map.ofList
+
+        let chxBalances =
+            [
+                ChainiumAddress "HH", {ChxBalanceState.Amount = ChxAmount 5M; Nonce = Nonce 7L}
+                ChainiumAddress "II", {ChxBalanceState.Amount = ChxAmount 6M; Nonce = Nonce 8L}
+            ]
+            |> Map.ofList
+
+        let holdings =
+            [
+                (AccountHash "DDD", EquityID "EEE"), {HoldingState.Amount = EquityAmount 1M; Nonce = Nonce 3L}
+                (AccountHash "FFF", EquityID "GGG"), {HoldingState.Amount = EquityAmount 2M; Nonce = Nonce 4L}
+            ]
+            |> Map.ofList
+
+        let processingOutput =
+            {
+                TxResults = txResults
+                ChxBalances = chxBalances
+                Holdings = holdings
+            }
+
+        let stateRoot =
+            [
+                "HH...E...................G" // CHX balance 1
+                "II...F...................H" // CHX balance 2
+                "DDDIII...A...................C" // Holding balance 1
+                "FFFAAA...B...................D" // Holding balance 2
+            ]
+            |> String.Concat
+
+        let blockHash =
+            [
+                ".......A" // blockNumber
+                "B" // previousBlockHash
+                ".......C" // timestamp
+                "D" // validator
+                "AAABBBCCC" // txSetRoot
+                "AAAABBBBCCCA" // txResultSetRoot
+                stateRoot
+            ]
+            |> String.Concat
+
+        // ACT
+        let block =
+            Blocks.assembleBlock
+                DummyHash.decode
+                DummyHash.create
+                DummyHash.merkleTree
+                validator
+                blockNumber
+                timestamp
+                previousBlockHash
+                txSet
+                processingOutput
+
+        // ASSERT
+        test <@ block.Header.Number = blockNumber @>
+        test <@ block.Header.PreviousHash = previousBlockHash @>
+        test <@ block.Header.Timestamp = timestamp @>
+        test <@ block.Header.Validator = validator @>
+        test <@ block.Header.TxSetRoot = MerkleTreeRoot "AAABBBCCC" @>
+        test <@ block.Header.TxResultSetRoot = MerkleTreeRoot "AAAABBBBCCCA" @>
+        test <@ block.Header.StateRoot = MerkleTreeRoot stateRoot @>
+        test <@ block.Header.Hash = BlockHash blockHash @>
+        test <@ block.TxSet = [TxHash "AAA"; TxHash "BBB"; TxHash "CCC"] @>
+
+    [<Fact>]
+    let ``Blocks.assembleBlock and verify merkle proofs`` () =
         let wallet1 = Signing.generateWallet None
         let wallet2 = Signing.generateWallet None
         let validatorWallet = Signing.generateWallet None
---
+++ Source/Chainium.Blockchain.Public.Core.Tests/DummyHash.fs
@@ -0,0 +1,50 @@
+namespace Chainium.Blockchain.Public.Core.Tests
+
+open System
+open System.Text
+open Newtonsoft.Json
+open Chainium.Common
+open Chainium.Blockchain.Common
+open Chainium.Blockchain.Public.Core
+open Chainium.Blockchain.Public.Core.DomainTypes
+open Chainium.Blockchain.Public.Core.Dtos
+open Chainium.Blockchain.Public.Crypto
+
+module DummyHash =
+
+    let create bytes =
+        bytes
+        |> Array.map (fun b ->
+            match b % 10uy with
+            | 0uy -> '.'
+            | 1uy -> 'A'
+            | 2uy -> 'B'
+            | 3uy -> 'C'
+            | 4uy -> 'D'
+            | 5uy -> 'E'
+            | 6uy -> 'F'
+            | 7uy -> 'G'
+            | 8uy -> 'H'
+            | 9uy -> 'I'
+            | b -> failwithf "%i is invalid value. Dymmy hash supports only bytes with values 0-9." b
+        )
+        |> String
+
+    let decode (str : string) =
+        str.ToCharArray()
+        |> Array.map (function
+            | '.' -> 0uy
+            | 'A' -> 1uy
+            | 'B' -> 2uy
+            | 'C' -> 3uy
+            | 'D' -> 4uy
+            | 'E' -> 5uy
+            | 'F' -> 6uy
+            | 'G' -> 7uy
+            | 'H' -> 8uy
+            | 'I' -> 9uy
+            | c -> failwithf "%s has invalid char %A. Dymmy hash supports only characters A-I and a dot for zero" str c
+        )
+
+    let merkleTree (hashes : string list) =
+        hashes |> String.Concat |> MerkleTreeRoot
