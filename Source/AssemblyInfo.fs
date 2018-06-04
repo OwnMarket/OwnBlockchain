a4379475194152abc833943f0d047d1e9b36ed9a
Anil Mujagic <anil.mujagic@chainium.io>
2018-06-04 15:47:10 +0200

    Version 0.2.0

--- Source/AssemblyInfo.fs
+++ Source/AssemblyInfo.fs
@@ -2,8 +2,8 @@ namespace Chainium.Blockchain.Public
 
 open System.Reflection
 
-[<assembly: AssemblyVersion("0.1.0.0")>]
-[<assembly: AssemblyFileVersion("0.1.0.0")>]
-[<assembly: AssemblyInformationalVersion("0.1.0")>]
+[<assembly: AssemblyVersion("0.2.0.0")>]
+[<assembly: AssemblyFileVersion("0.2.0.0")>]
+[<assembly: AssemblyInformationalVersion("0.2.0")>]
 
 ()
