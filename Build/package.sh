2ebb8f38dbaca4fd4487851a608281d04dc92b38
MrPink <sead.pavica@chainium.io>
2018-06-05 11:58:43 +0200

    changed published file name, updated packaging script

--- Build/package.sh
+++ Build/package.sh
@@ -17,11 +17,11 @@ do
 	cp -R -f $SOLUTION_DIR/$PROJ/$PROJECT_OUTPUT_FOLDER/* $PUBLISH_DIRECTORY/$PROJ
 done
 
-tar -zcvf $PUBLISH_DIRECTORY/$PACKAGE_NAME.tar.gz $PUBLISH_DIRECTORY/*
+(cd $PUBLISH_DIRECTORY && tar -zcvf $PUBLISH_DIRECTORY/$PACKAGE_NAME.tar.gz *)
 
 if `command -v zip >/dev/null`
 then
-	find $PUBLISH_DIRECTORY/* ! -name "*.tar.gz" | zip $PUBLISH_DIRECTORY/$PACKAGE_NAME.zip -@
+	find $PUBLISH_DIRECTORY ! -name "*.tar.gz" | zip -r $PUBLISH_DIRECTORY/$PACKAGE_NAME.zip -@
 else
 	echo "Zip is not installed. Unable to create zip archive."
 fi
