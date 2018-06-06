efe1f76e354bca67a6098009263acb63038ddfa2
Anil Mujagic <anil.mujagic@chainium.io>
2018-06-06 13:17:44 +0200

    Avoid parentheses

--- Dev/CheckCodeStyle.fsx
+++ Dev/CheckCodeStyle.fsx
@@ -44,197 +44,149 @@ let rules =
         // Empty lines
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        createRule (
-            function
+        createRule <| function
             | None, Some line when line.IsEmpty ->
                 Some "There should be no empty lines at the top of the file."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | Some line, None when line.IsEmpty ->
                 Some "There should be no empty lines at the bottom of the file."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | Some line1, Some line2 when (line1.IsEmpty) && (line2.IsEmpty) ->
                 Some "There should be no multiple consecutive empty lines."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | Some line1, Some line2 when line1.Trim().StartsWith("module ") && not line2.IsEmpty ->
                 Some "There should be an empty line after module declaration."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | Some line1, Some line2 when line1.TrimStart().StartsWith("let ") && line2.Trim() = "=" ->
                 Some "Equal sign should be on a separate line only in multiline function declaration."
             | Some line1, Some line2 when line1.Trim() = "=" && not line2.IsEmpty ->
                 Some "There should be an empty line after multiline function declaration."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | Some line1, Some line2 when line1.Trim().EndsWith("->") && line2.IsEmpty ->
                 Some "There should be no empty lines after function arrow."
             | _ -> None
-        )
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         // Spaces
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Trim().Contains("  ") ->
                 Some "There should be no multiple consecutive spaces in a line."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.EndsWith(" ") ->
                 Some "There should be no spaces at the end of a line."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Contains(" ,") ->
                 Some "There should be no space before comma."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Contains(" ;") ->
                 Some "There should be no space before semicolon."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Contains("( ") ->
                 Some "There should be no space after open parentheses."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\,\w") ->
                 Some "There should be a space after comma."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\;\w") ->
                 Some "There should be a space after semicolon."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\w\=") || Regex.IsMatch(line, "\=\w") ->
                 Some "There should be a space before and after the equal sign."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\w\:\:") || Regex.IsMatch(line, "\:\:\w") ->
                 Some "There should be a space before and after the cons '::' operator."
             | _ -> None
-        )
 
         (*
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\w\:") || Regex.IsMatch(line, "\:\w") ->
                 Some "There should be a space before and after colon sign."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Trim().EndsWith(";") ->
                 Some "There should be no semicolon at the end of the line."
             | _ -> None
-        )
         *)
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         // Intentation
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Contains("\t") ->
                 Some "Don't use tabs, use four spaces instead."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when (line.Indentation % 4) <> 0 ->
                 Some "Use four spaces per indentation level."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | Some line1, Some line2 when
                 not (line1.IsEmpty)
                 && line2.Indentation > line1.Indentation
                 && line2.Indentation - line1.Indentation <> 4 ->
                 Some "Don't indent for more than one level at a time."
             | _ -> None
-        )
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         // Length
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Trim().Length > 120 ->
                 Some "Line should not be longer than 120 characters."
             | _ -> None
-        )
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when line.Contains("////") && line.Trim().Length <> 100 ->
                 Some "Section header should have 100 slashes."
             | _ -> None
-        )
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         // Comments
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\/\/\w") && not (Regex.IsMatch(line, "\:\/\/\w")) ->
                 Some "There should be a space between comment slashes and comment text."
             | _ -> None
-        )
 
         (*
-        createRule (
-            function
+        createRule <| function
             | _, Some line when Regex.IsMatch(line, "\/\/ [a-z]") || Regex.IsMatch(line, "\/\/[a-z]") ->
                 Some "Comment text should start with capital letter."
             | _ -> None
-        )
         *)
     ]
 
