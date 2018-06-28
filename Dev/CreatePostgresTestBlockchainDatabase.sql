3bb440b3d69c5b37dc5d80f2874f8cf909f82343
Anil Mujagic <anil.mujagic@chainium.io>
2018-06-28 12:15:09 +0200

    Fix test DB creation script and sync it with conn string

--- Dev/CreatePostgresTestBlockchainDatabase.sql
+++ Dev/CreatePostgresTestBlockchainDatabase.sql
@@ -6,25 +6,25 @@ CREATE USER chainium_tests WITH PASSWORD 'testpass1';
 
 CREATE DATABASE chainium_public_blockchain;
 \c chainium_public_blockchain
- 
+
 SET search_path TO public;
- 
+
 -- Create extensions
 CREATE EXTENSION adminpack;
- 
+
 -- Create schemas
 CREATE SCHEMA IF NOT EXISTS chainium;
- 
+
 -- Set default permissions
 ALTER DEFAULT PRIVILEGES
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO chainium_tests;
- 
+
 ALTER DEFAULT PRIVILEGES
 GRANT SELECT, USAGE ON SEQUENCES TO chainium_tests;
- 
+
 -- Set permissions on schemas
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT USAGE ON SCHEMA public TO chainium_tests;
- 
+
 GRANT ALL ON SCHEMA chainium TO postgres;
-GRANT USAGE ON SCHEMA chainium TO chainium_tests;
+GRANT USAGE, CREATE ON SCHEMA chainium TO chainium_tests;
