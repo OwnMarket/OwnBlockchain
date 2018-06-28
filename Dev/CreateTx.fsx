501177d784e3cdc5d9e7ebd3817bd39b55d0e7b5
Anil Mujagic <anil.mujagic@chainium.io>
2018-06-28 06:42:48 +0200

    Add sample txs in CreateTx script

--- Dev/CreateTx.fsx
+++ Dev/CreateTx.fsx
@@ -6,7 +6,7 @@ open System.Text
 """
 {
     Nonce: 1,
-    Fee: 1,
+    Fee: 0.1,
     Actions: [
         {
             ActionType: "TransferChx",
@@ -31,4 +31,20 @@ Address: CHQcJKysWbbqyRm5ho44jexA8radTZzNQQ2
 V: 1
 R: 1FdmCDScepMzMbkYwwF6nupAgE5WqhLcWjFUjBm11kjVa
 S: bGPah3wtfWmC6RuNN3Ur1MzMNAutYF1ehcq3DoVUBWZ
+
+// 10 CHX to CHKj2rtVz5z6Pyy1bpH8GaEyhTX7eabQzdw
+{
+    "tx": "CnsKICAgIE5vbmNlOiAxLAogICAgRmVlOiAwLjEsCiAgICBBY3Rpb25zOiBbCiAgICAgICAgewogICAgICAgICAgICBBY3Rpb25UeXBlOiAiQ2h4VHJhbnNmZXIiLAogICAgICAgICAgICBBY3Rpb25EYXRhOiB7CiAgICAgICAgICAgICAgICBSZWNpcGllbnRBZGRyZXNzOiAiQ0hLajJydFZ6NXo2UHl5MWJwSDhHYUV5aFRYN2VhYlF6ZHciLAogICAgICAgICAgICAgICAgQW1vdW50OiAxMAogICAgICAgICAgICB9CiAgICAgICAgfQogICAgXQp9Cg==",
+    "v": "2",
+    "r": "6Mi4QHn5BCVeycvPMHMUqP7hEiZeGR76dC4YvPYytGpD",
+    "s": "7sQpvc6Awj1XkSG8ppDtFC4qd9wpTG5tCDtfTK8KfH5o"
+}
+
+// 5 CHX to CHen2J21nxRj1rQhwQWBXfehs97YJ2TJVoC
+{
+    "tx": "CnsKICAgIE5vbmNlOiAyLAogICAgRmVlOiAwLjEsCiAgICBBY3Rpb25zOiBbCiAgICAgICAgewogICAgICAgICAgICBBY3Rpb25UeXBlOiAiQ2h4VHJhbnNmZXIiLAogICAgICAgICAgICBBY3Rpb25EYXRhOiB7CiAgICAgICAgICAgICAgICBSZWNpcGllbnRBZGRyZXNzOiAiQ0hlbjJKMjFueFJqMXJRaHdRV0JYZmVoczk3WUoyVEpWb0MiLAogICAgICAgICAgICAgICAgQW1vdW50OiA1CiAgICAgICAgICAgIH0KICAgICAgICB9CiAgICBdCn0K",
+    "v": "2",
+    "r": "1DaEpxNa3vKqXVUn7e6tWTGLqcVKvqku9KGGffqaLcZtW",
+    "s": "1Fcr7Yhc6KZeQhT8DPaKoH4JkupMn2VrPc3gPGw4Vywm4"
+}
 *)
