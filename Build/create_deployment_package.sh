1ab67399637f8df11f58dd78267e8a4bef090fd5
Anil Mujagic <anil.mujagic@weown.com>
2018-09-28 11:13:30 +0200

    Add packaging and deployment scripts

---
+++ Build/create_deployment_package.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Prevent packing of resource fork files on macOS (./._xxxxx)
+export COPYFILE_DISABLE=true
+
+set -e
+
+pushd "${0%/*}" # Go to script directory
+BUILD_DIR="$(pwd)"
+TEMP_DIR="$BUILD_DIR/Temp"
+OUTPUT_DIR="$BUILD_DIR/Output"
+
+rm -rf "$TEMP_DIR"
+mkdir -p -m 777 "$TEMP_DIR"
+
+rm -rf "$OUTPUT_DIR"
+mkdir -p -m 777 "$OUTPUT_DIR"
+
+# Build the Node
+pushd ../Source/Chainium.Blockchain.Public.Node
+mkdir -p "$TEMP_DIR/Node"
+dotnet publish -c Release -o "$TEMP_DIR/Node"
+popd
+
+cp -r ~/.nuget/packages/secp256k1.net/0.1.48/content/native "$TEMP_DIR/Node"
+cp -r ../Docs/Deployment/setup_public_node.sh "$TEMP_DIR/Node"
+cp -r ../Docs/Deployment/setup_public_node_instance.sh "$TEMP_DIR/Node"
+
+pushd "$TEMP_DIR/Node"
+chmod +x *.sh
+tar czf "$OUTPUT_DIR/Node.tar.gz" *
+popd
+
+# Build the Faucet
+pushd ../Source/Chainium.Blockchain.Public.Faucet
+mkdir -p "$TEMP_DIR/Faucet"
+dotnet publish -c Release -o "$TEMP_DIR/Faucet"
+popd
+
+cp -r ~/.nuget/packages/secp256k1.net/0.1.48/content/native "$TEMP_DIR/Faucet"
+
+pushd "$TEMP_DIR/Faucet"
+tar czf "$OUTPUT_DIR/Faucet.tar.gz" *
+popd
+
+# Cleanup
+rm -rf "$TEMP_DIR"
+
+# Restore calling location
+popd
+
+# Show the output files
+echo '>>> Packages are in following location:'
+echo "$OUTPUT_DIR"
+ls "$OUTPUT_DIR"
---
+++ Docs/Deployment/setup_public_node.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+cd "${0%/*}"
+
+echo '////////////////////////////////////////////////////////////////////////////////'
+echo '// Postgres'
+echo '////////////////////////////////////////////////////////////////////////////////'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt update -y
+sudo apt install -y postgresql-10 postgresql-contrib-10
+sudo -u postgres psql -c 'CREATE EXTENSION adminpack'
+
+echo '////////////////////////////////////////////////////////////////////////////////'
+echo '// .NET Core'
+echo '////////////////////////////////////////////////////////////////////////////////'
+wget -q -O /tmp/packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
+sudo apt install -y apt-transport-https
+sudo apt update -y
+sudo apt install -y dotnet-sdk-2.1
+
+echo '////////////////////////////////////////////////////////////////////////////////'
+echo '// Node binaries'
+echo '////////////////////////////////////////////////////////////////////////////////'
+NODE_DIR=/opt/own/blockchain/public/node
+DATA_DIR=/var/lib/own/blockchain/public/node
+sudo mkdir -p -m +r "$NODE_DIR"
+sudo mkdir -p "$DATA_DIR"
+sudo cp -r ./* "$NODE_DIR"
+
+echo '////////////////////////////////////////////////////////////////////////////////'
+echo '// Systemd service unit'
+echo '////////////////////////////////////////////////////////////////////////////////'
+sudo bash -c 'cat > /lib/systemd/system/own-blockchain-public-node@.service' << EOF
+[Unit]
+Description=Own Public Blockchain Node
+After=network.target postgresql.service
+
+[Service]
+WorkingDirectory=$DATA_DIR/%i
+ExecStart=/usr/bin/dotnet "$NODE_DIR/Chainium.Blockchain.Public.Node.dll"
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+
+echo '////////////////////////////////////////////////////////////////////////////////'
+echo '// Instance 1'
+echo '////////////////////////////////////////////////////////////////////////////////'
+./setup_public_node_instance.sh ins1
