70dae3d2e5bc71691b4d73d4ca4dabeb88968d9d
Anil Mujagic <anil.mujagic@weown.com>
2018-11-07 14:12:15 +0100

    Separate PG database initialization scripts for integration tests and normal use

--- Dev/CreatePostgresBlockchainDatabase.sql
+++ Dev/CreatePostgresBlockchainDatabase.sql
@@ -2,7 +2,7 @@
 
 \c postgres
 
-CREATE USER own_tests WITH PASSWORD 'testpass1';
+CREATE USER own_blockchain_node WITH PASSWORD 'testpass1';
 
 CREATE DATABASE own_public_blockchain;
 \c own_public_blockchain
@@ -10,21 +10,21 @@ CREATE DATABASE own_public_blockchain;
 SET search_path TO public;
 
 -- Create extensions
-CREATE EXTENSION adminpack;
+--CREATE EXTENSION adminpack;
 
 -- Create schemas
 CREATE SCHEMA IF NOT EXISTS own;
 
 -- Set default permissions
 ALTER DEFAULT PRIVILEGES
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO own_tests;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO own_blockchain_node;
 
 ALTER DEFAULT PRIVILEGES
-GRANT SELECT, USAGE ON SEQUENCES TO own_tests;
+GRANT SELECT, USAGE ON SEQUENCES TO own_blockchain_node;
 
 -- Set permissions on schemas
 GRANT ALL ON SCHEMA public TO postgres;
-GRANT USAGE ON SCHEMA public TO own_tests;
+GRANT USAGE ON SCHEMA public TO own_blockchain_node;
 
 GRANT ALL ON SCHEMA own TO postgres;
-GRANT USAGE, CREATE ON SCHEMA own TO own_tests;
+GRANT USAGE, CREATE ON SCHEMA own TO own_blockchain_node;
---
+++ Dev/CreatePostgresBlockchainDatabaseForTests.sql
@@ -0,0 +1,30 @@
+\set ON_ERROR_STOP on
+
+\c postgres
+
+CREATE USER own_blockchain_tests WITH PASSWORD 'testpass1';
+
+CREATE DATABASE own_public_blockchain_tests;
+\c own_public_blockchain_tests
+
+SET search_path TO public;
+
+-- Create extensions
+--CREATE EXTENSION adminpack;
+
+-- Create schemas
+CREATE SCHEMA IF NOT EXISTS own;
+
+-- Set default permissions
+ALTER DEFAULT PRIVILEGES
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO own_blockchain_tests;
+
+ALTER DEFAULT PRIVILEGES
+GRANT SELECT, USAGE ON SEQUENCES TO own_blockchain_tests;
+
+-- Set permissions on schemas
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT USAGE ON SCHEMA public TO own_blockchain_tests;
+
+GRANT ALL ON SCHEMA own TO postgres;
+GRANT USAGE, CREATE ON SCHEMA own TO own_blockchain_tests;
