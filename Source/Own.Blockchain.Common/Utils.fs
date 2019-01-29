11df31f420fdeb0cdd7e0de09c5862b9faf013e3
Anil Mujagic <anil.mujagic@weown.com>
2019-01-29 11:11:25 +0100

    Clarify the purpose of the functions

--- Source/Own.Blockchain.Common/Utils.fs
+++ Source/Own.Blockchain.Common/Utils.fs
@@ -8,9 +8,11 @@ module Utils =
     let getUnixTimestamp () =
         System.DateTimeOffset.UtcNow.ToUnixTimeSeconds()
 
+    /// Truncates a decimal number to blockchain default of 7 decimal digits.
     let round (x : decimal) =
         let multiplier = 10_000_000m // 7 decimal places
         Math.Truncate(x * multiplier) / multiplier
 
+    /// Checks if a decimal number is truncated to blockchain default of 7 decimal digits.
     let isRounded x =
         x = round x
