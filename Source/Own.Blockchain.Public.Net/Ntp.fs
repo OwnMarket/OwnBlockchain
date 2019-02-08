7481b90fd0150689873454758b7cd9da080c3125
Anil Mujagic <anil.mujagic@weown.com>
2019-02-08 11:28:22 +0100

    Fetch time from NTP server

--- Source/Own.Blockchain.Public.Net/Ntp.fs
+++ Source/Own.Blockchain.Public.Net/Ntp.fs
@@ -1,14 +1,41 @@
 namespace Own.Blockchain.Public.Net
 
+open System
+open System.Net
+open GuerrillaNtp
+open Own.Common
+open Own.Blockchain.Common
+
 module Ntp =
 
     let ntpServers =
         [
-            ""
-            ""
-            ""
+            "pool.ntp.org"
+            "europe.pool.ntp.org"
+            "north-america.pool.ntp.org"
+            "asia.pool.ntp.org"
+            "oceania.pool.ntp.org"
+            "south-america.pool.ntp.org"
+            "africa.pool.ntp.org"
+            "time.google.com" // Uses leap second smearing
+            "time.windows.com"
+            "time.apple.com"
+            "time.nist.gov"
         ]
+
+    let private getNetworkTimeOffsetFromNtpServer ntpServerName =
+        try
+            Log.infof "Fetching time from NTP server: %s" ntpServerName
+            use ntp = new NtpClient(Dns.GetHostAddresses(ntpServerName).[0])
+            ntp.GetCorrectionOffset().TotalMilliseconds |> Convert.ToInt64 |> Some
+        with
+        | ex ->
+            Log.error ex.AllMessages
+            None
 
     /// Returns number of milliseconds network time is ahead (+) or behind (-) local machine time.
     let getNetworkTimeOffset () =
-        0L // TODO: Implement
+        ntpServers
+        |> Seq.choose getNetworkTimeOffsetFromNtpServer
+        |> Seq.tryHead
+        |? 0L
