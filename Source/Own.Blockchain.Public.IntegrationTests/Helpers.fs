ba6d005f8cdf9e52aaf14b32d917b58721a47d65
Anil Mujagic <anil.mujagic@weown.com>
2019-02-26 22:36:40 +0100

    Setup new integration test infrastructure

---
+++ Source/Own.Blockchain.Public.IntegrationTests/Helpers.fs
@@ -0,0 +1,112 @@
+namespace Own.Blockchain.Public.IntegrationTests
+
+open System.IO
+open System.Data
+open Own.Blockchain.Public.Data
+open Own.Blockchain.Public.Node
+
+module Helpers =
+    open Own.Blockchain.Public.Core.Dtos
+
+    let dbExecute =
+        DbTools.execute Config.DbEngineType Config.DbConnectionString
+
+    let private runInDbTransaction f =
+        use conn = DbTools.newConnection Config.DbEngineType Config.DbConnectionString
+
+        conn.Open()
+        use transaction = conn.BeginTransaction(IsolationLevel.ReadCommitted)
+
+        f transaction |> ignore
+
+        transaction.Commit()
+        conn.Close()
+
+    let resetTestData () =
+        let schemaName = (Npgsql.NpgsqlConnectionStringBuilder Config.DbConnectionString).SearchPath
+
+        let removeAllTables =
+            sprintf
+                """
+                DO $$ DECLARE
+                    v_table_name TEXT;
+                BEGIN
+                    FOR v_table_name IN
+                        SELECT tablename
+                        FROM pg_tables
+                        WHERE schemaname = '%s'
+                    LOOP
+                        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(v_table_name) || ' CASCADE';
+                    END LOOP;
+                END $$;
+                """
+                schemaName
+
+        dbExecute removeAllTables [] |> ignore
+
+        if Directory.Exists Config.DataDir then
+            Directory.Delete(Config.DataDir, true)
+
+        DbInit.init Config.DbEngineType Config.DbConnectionString
+
+    let addChxBalance (chxBalanceInfo : ChxBalanceInfoDto) =
+        let sql =
+            """
+            INSERT INTO chx_balance (blockchain_address, amount, nonce)
+            VALUES (@blockchainAddress, @amount, @nonce)
+            """
+
+        [
+            "@blockchainAddress", chxBalanceInfo.BlockchainAddress |> box
+            "@amount", chxBalanceInfo.ChxBalanceState.Amount |> box
+            "@nonce", chxBalanceInfo.ChxBalanceState.Nonce |> box
+        ]
+        |> dbExecute sql
+        |> ignore
+
+    let addValidator (validatorInfo : ValidatorInfoDto) =
+        let sql =
+            """
+            INSERT INTO validator (
+                validator_address,
+                network_address,
+                shared_reward_percent,
+                time_to_lock_deposit,
+                time_to_blacklist,
+                is_enabled
+            )
+            VALUES (
+                @validatorAddress,
+                @networkAddress,
+                @sharedRewardPercent,
+                @timeToLockDeposit,
+                @timeToBlacklist,
+                @isEnabled
+            )
+            """
+
+        [
+            "@validatorAddress", validatorInfo.ValidatorAddress |> box
+            "@networkAddress", validatorInfo.NetworkAddress |> box
+            "@sharedRewardPercent", validatorInfo.SharedRewardPercent |> box
+            "@timeToLockDeposit", validatorInfo.TimeToLockDeposit |> box
+            "@timeToBlacklist", validatorInfo.TimeToBlacklist |> box
+            "@isEnabled", validatorInfo.IsEnabled |> box
+        ]
+        |> dbExecute sql
+        |> ignore
+
+    let addStake (stakeInfo : StakeInfoDto) =
+        let sql =
+            """
+            INSERT INTO stake (staker_address, validator_address, amount)
+            VALUES (@stakerAddress, @validatorAddress, @amount)
+            """
+
+        [
+            "@stakerAddress", stakeInfo.StakerAddress |> box
+            "@validatorAddress", stakeInfo.ValidatorAddress |> box
+            "@amount", stakeInfo.StakeState.Amount |> box
+        ]
+        |> dbExecute sql
+        |> ignore
---
+++ Source/Own.Blockchain.Public.IntegrationTests/ValidatorTests.fs
@@ -0,0 +1,66 @@
+namespace Own.Blockchain.Public.IntegrationTests
+
+open Xunit
+open Swensen.Unquote
+open Own.Blockchain.Public.Core.DomainTypes
+open Own.Blockchain.Public.Core.Dtos
+open Own.Blockchain.Public.Crypto
+open Own.Blockchain.Public.Node
+
+module ValidatorTests =
+
+    [<Fact>]
+    let ``Validator not included in the config if the available balance is less than required deposit`` () =
+        // ARRANGE
+        Helpers.resetTestData ()
+
+        let validator1Wallet = Signing.generateWallet ()
+        let validator2Wallet = Signing.generateWallet ()
+        let stakerWallet = Signing.generateWallet ()
+
+        [
+            validator1Wallet.Address.Value, Config.ValidatorDeposit
+            validator2Wallet.Address.Value, Config.ValidatorDeposit - 1m // Won't have sufficient deposit.
+        ]
+        |> List.iter (fun (address, deposit) ->
+            {
+                BlockchainAddress = address
+                ChxBalanceState =
+                    {
+                        ChxBalanceStateDto.Nonce = 0L
+                        Amount = deposit
+                    }
+            }
+            |> Helpers.addChxBalance
+
+            {
+                ValidatorAddress = address
+                NetworkAddress = sprintf "%s.weown.com:25718" address
+                SharedRewardPercent = 0m
+                TimeToLockDeposit = 0s
+                TimeToBlacklist = 0s
+                IsEnabled = true
+            }
+            |> Helpers.addValidator
+
+            {
+                StakeInfoDto.StakerAddress = stakerWallet.Address.Value
+                ValidatorAddress = address
+                StakeState =
+                    {
+                        Amount = 500_000m
+                    }
+            }
+            |> Helpers.addStake
+        )
+
+        // ACT
+        let topValidators =
+            Composition.getTopValidatorsByStake
+                Config.MaxValidatorCount
+                (ChxAmount Config.ValidatorThreshold)
+                (ChxAmount Config.ValidatorDeposit)
+
+        // ASSERT
+        test <@ topValidators.Length = 1 @>
+        test <@ topValidators.[0].ValidatorAddress = validator1Wallet.Address.Value @>
