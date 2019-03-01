898ac0773bf05a8fa4cb1238548d502ade9de87d
Anil Mujagic <anil.mujagic@weown.com>
2019-03-01 02:11:39 +0100

    Enable running tests with Firebird

--- Testing/start_test_nodes.sh
+++ Testing/start_test_nodes.sh
@@ -4,24 +4,31 @@ set -e
 
 pushd "${0%/*}" # Go to script directory
 cd TestInstances
-APP_COMMAND="dotnet ../../../Source/Own.Blockchain.Public.Node/bin/Release/netcoreapp2.1/Own.Blockchain.Public.Node.dll"
+
+APP_DIR="../../../Source/Own.Blockchain.Public.Node/bin/Release/netcoreapp2.1"
+APP_COMMAND="dotnet $APP_DIR/Own.Blockchain.Public.Node.dll"
+FIREBIRD_DIR="$(cd Ins1 && cd $APP_DIR && pwd)"
 
 tmux new-session -s "blockchain-test" -d
 
 tmux split-window -v -d
 tmux send-keys "cd Ins1" C-m
+tmux send-keys "export FIREBIRD=$FIREBIRD_DIR" C-m
 tmux send-keys "$APP_COMMAND" C-m
 
 tmux split-window -h
 tmux send-keys "cd Ins2" C-m
+tmux send-keys "export FIREBIRD=$FIREBIRD_DIR" C-m
 tmux send-keys "$APP_COMMAND" C-m
 
 tmux select-pane -D
 tmux send-keys "cd Ins3" C-m
+tmux send-keys "export FIREBIRD=$FIREBIRD_DIR" C-m
 tmux send-keys "$APP_COMMAND" C-m
 
 tmux split-window -h
 tmux send-keys "cd Ins4" C-m
+tmux send-keys "export FIREBIRD=$FIREBIRD_DIR" C-m
 tmux send-keys "$APP_COMMAND" C-m
 
 tmux -2 -CC attach-session -d
