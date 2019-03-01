043859d0ca4b99f1b56ce73f37be3f3758ace39f
Anil Mujagic <anil.mujagic@weown.com>
2019-03-01 02:31:16 +0100

    Introduce log levels

--- Source/Own.Blockchain.Common/Log.fs
+++ Source/Own.Blockchain.Common/Log.fs
@@ -6,6 +6,16 @@ open Own.Blockchain.Common
 
 module Log =
 
+    type LogLevel =
+        | Debug = 0
+        | Info = 1
+        | Notice = 2
+        | Success = 3
+        | Warning = 4
+        | Error = 5
+
+    let mutable minLogLevel = LogLevel.Debug
+
     let private defaultColor = Console.ForegroundColor
 
     let private cts = new CancellationTokenSource()
@@ -38,32 +48,38 @@ module Log =
 
     /// Errors which prevented successful execution.
     let error o =
-        log "ERR" o |> printInColor ConsoleColor.Red
+        if minLogLevel <= LogLevel.Error then
+            log "ERR" o |> printInColor ConsoleColor.Red
 
     /// Events that are potentially problematic but didn't prevent the successful execution.
     /// (e.g. not being able to propose block due to not having latest block applied to the state yet)
     let warning o =
-        log "WRN" o |> printInColor ConsoleColor.Yellow
+        if minLogLevel <= LogLevel.Warning then
+            log "WRN" o |> printInColor ConsoleColor.Yellow
 
     /// Important successful events.
     /// (e.g. block applied to the state)
     let success o =
-        log "SUC" o |> printInColor ConsoleColor.Green
+        if minLogLevel <= LogLevel.Success then
+            log "SUC" o |> printInColor ConsoleColor.Green
 
     /// Important unordinary events.
     /// (e.g. applying DB change; saving TxResult to the disk during processing)
     let notice o =
-        log "NOT" o |> printInColor ConsoleColor.Cyan
+        if minLogLevel <= LogLevel.Notice then
+            log "NOT" o |> printInColor ConsoleColor.Cyan
 
     /// Ordinary events.
     /// (e.g. Tx submitted; block received)
     let info o =
-        log "INF" o |> printInColor ConsoleColor.White
+        if minLogLevel <= LogLevel.Info then
+            log "INF" o |> printInColor ConsoleColor.White
 
     /// Detailed info for debugging purpose.
     let debug o =
         #if DEBUG
-        log "DBG" o |> printInColor ConsoleColor.DarkGray
+        if minLogLevel <= LogLevel.Debug then
+            log "DBG" o |> printInColor ConsoleColor.DarkGray
         #else
         ()
         #endif
