019bce4d57e0c2399e0f11718a85e9ff2073c522
Anil Mujagic <anil.mujagic@weown.com>
2019-03-15 19:45:44 +0100

    Version 0.8.1
    (cherry picked from commit 84c98d71d59229633206921d76f97738b21c33f5)

--- Source/AssemblyInfo.fs
+++ Source/AssemblyInfo.fs
@@ -2,8 +2,8 @@ namespace Own.Blockchain.Public
 
 open System.Reflection
 
-[<assembly: AssemblyVersion("0.8.0.0")>]
-[<assembly: AssemblyFileVersion("0.8.0.0")>]
-[<assembly: AssemblyInformationalVersion("0.8.0")>]
+[<assembly: AssemblyVersion("0.8.1.0")>]
+[<assembly: AssemblyFileVersion("0.8.1.0")>]
+[<assembly: AssemblyInformationalVersion("0.8.1")>]
 
 ()
