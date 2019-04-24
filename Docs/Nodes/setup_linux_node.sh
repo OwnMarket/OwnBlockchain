a54c0d5c1b5ab2c6f11449614c7b1ff229dc4a92
Anil Mujagic <anil.mujagic@weown.com>
2019-04-25 00:50:07 +0200

    Add script for invoking node setup on Linux

---
+++ Docs/Nodes/setup_linux_node.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+SETUP_DIR=/tmp/OwnPublicBlockchainNodeSetup
+
+# Prepare temp dir
+rm -rf "$SETUP_DIR"
+mkdir -p -m 777 "$SETUP_DIR"
+cd "$SETUP_DIR"
+
+# Download the package
+wget https://github.com/OwnMarket/OwnBlockchain/releases/latest/download/OwnPublicBlockchainNode_linux-x64.tar.gz
+
+# Extract the package
+mkdir Package
+cd Package
+tar xzf ../OwnPublicBlockchainNode_linux-x64.tar.gz
+
+# Setup the node
+./setup_node.sh
