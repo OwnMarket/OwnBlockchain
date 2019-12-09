699bc7dcf455280a48c1a2d04379342c9ff64646
Anil Mujagic <anil.mujagic@weown.com>
2019-12-09 18:09:08 +0100

    Reject TX if nonce is not greater than current address nonce

--- Source/Own.Blockchain.Public.Core/Workflows.fs
+++ Source/Own.Blockchain.Public.Core/Workflows.fs
@@ -1486,6 +1486,7 @@ module Workflows =
         isValidHash
         isValidAddress
         createHash
+        (getChxAddressState : BlockchainAddress -> ChxAddressStateDto option)
         getAvailableChxBalance
         getTotalFeeForPendingTxs
         saveTx
@@ -1505,6 +1506,17 @@ module Workflows =
 
             let! txDto = Serialization.deserializeTx txEnvelope.RawTx
             let! tx = Validation.validateTx isValidHash isValidAddress maxActionCountPerTx senderAddress txHash txDto
+
+            let addressNonce =
+                getChxAddressState tx.Sender
+                |> Option.map (fun s -> s.Nonce)
+                |? 0L
+                |> Nonce
+            if tx.Nonce <= addressNonce then
+                return!
+                    addressNonce.Value
+                    |> sprintf "TX nonce must be greater than current address nonce: %i"
+                    |> Result.appError
 
             // TXs included in verified blocks are considered to be valid, hence shouldn't be rejected for fees.
             if not isIncludedInBlock then
