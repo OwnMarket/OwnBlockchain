b8a4b204551f163f62e27b8ca04047b8aa36f253
Anil Mujagic <anil.mujagic@weown.com>
2019-12-28 13:04:32 +0100

    Version 1.4.5

--- Source/AssemblyInfo.fs
+++ Source/AssemblyInfo.fs
@@ -2,8 +2,8 @@ namespace Own.Blockchain.Public
 
 open System.Reflection
 
-[<assembly: AssemblyVersion("1.4.4.0")>]
-[<assembly: AssemblyFileVersion("1.4.4.0")>]
-[<assembly: AssemblyInformationalVersion("1.4.4")>]
+[<assembly: AssemblyVersion("1.4.5.0")>]
+[<assembly: AssemblyFileVersion("1.4.5.0")>]
+[<assembly: AssemblyInformationalVersion("1.4.5")>]
 
 ()
