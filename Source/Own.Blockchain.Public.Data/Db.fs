6fce225e13f134c60ef253c891c221dc9ec8c90d
GitBot <gitbot@weown.com>
2020-01-10 16:42:08 +0100

    Don't remove TXs already included in the block
    (cherry picked from commit 3fc283ff1f243a098429d0103f3204233e59f1c6)

--- Source/Own.Blockchain.Public.Data/Db.fs
+++ Source/Own.Blockchain.Public.Data/Db.fs
@@ -229,6 +229,7 @@ module Db =
             """
             DELETE FROM tx
             WHERE action_fee < @minActionFee
+            AND NOT is_fetched
             RETURNING tx_hash
             """
 
