8255ce9d844a8233a48d25820983d3c150545940
GitBot <gitbot@weown.com>
2020-04-06 22:40:49 +0200

    Add forking logic
    (cherry picked from commit 20376091e53ae4c8d82549f877595301056b33aa)

---
+++ Source/Own.Blockchain.Public.Core/Forks.fs
@@ -0,0 +1,62 @@
+namespace Own.Blockchain.Public.Core
+
+open System
+open Own.Common.FSharp
+open Own.Blockchain.Public.Core.DomainTypes
+
+module Forks =
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Fork types - each fork has its own record type containing all the properties related to it.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    type DormantValidatorsFork = {
+        BlockNumber : BlockNumber
+    }
+
+type Forks () =
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Forks
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    static let mutable _dormantValidatorsFork : Forks.DormantValidatorsFork option = None
+
+    // Initialize all forks
+    static member Init networkCode =
+
+        _dormantValidatorsFork <-
+            match networkCode with
+            | "OWN_PUBLIC_BLOCKCHAIN_MAINNET" ->
+                {
+                    Forks.DormantValidatorsFork.BlockNumber = BlockNumber Int64.MaxValue // TODO FORK: Set for release
+                }
+                |> Some
+            | "OWN_PUBLIC_BLOCKCHAIN_TESTNET" ->
+                {
+                    Forks.DormantValidatorsFork.BlockNumber = BlockNumber Int64.MaxValue // TODO FORK: Set for release
+                }
+                |> Some
+            | "OWN_PUBLIC_BLOCKCHAIN_DEVNET" ->
+                {
+                    Forks.DormantValidatorsFork.BlockNumber = BlockNumber 1L
+                }
+                |> Some
+            | "UNIT_TESTS" ->
+                {
+                    Forks.DormantValidatorsFork.BlockNumber = BlockNumber 6L
+                }
+                |> Some
+            | _ ->
+                {
+                    Forks.DormantValidatorsFork.BlockNumber = BlockNumber 1L
+                }
+                |> Some
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Accessors
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    static member DormantValidators
+        with get () =
+            _dormantValidatorsFork |?> fun _ -> failwith "DormantValidatorsFork not initialized"
