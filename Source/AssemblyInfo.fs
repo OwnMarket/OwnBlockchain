d57452de46323b7394008d2176f413b5743a1c9a
GitBot <gitbot@weown.com>
2020-04-19 12:37:54 +0200

    Version 1.5.2

--- Source/AssemblyInfo.fs
+++ Source/AssemblyInfo.fs
@@ -2,8 +2,8 @@ namespace Own.Blockchain.Public
 
 open System.Reflection
 
-[<assembly: AssemblyVersion("1.5.1.0")>]
-[<assembly: AssemblyFileVersion("1.5.1.0")>]
-[<assembly: AssemblyInformationalVersion("1.5.1")>]
+[<assembly: AssemblyVersion("1.5.2.0")>]
+[<assembly: AssemblyFileVersion("1.5.2.0")>]
+[<assembly: AssemblyInformationalVersion("1.5.2")>]
 
 ()
